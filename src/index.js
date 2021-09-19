94bbd8afdfdc10f7fbde41a5692f47f7a4cc118c
shivkumarprajapat <shivkumarprajapat28@gmail.com>
2021-09-19 16:34:07 +0530

    sidebar section is completed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/css/index.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './assets/css/grid.css'
+import Layout from './components/layout/Layout'
 
+document.title ="React DashBorad"
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
